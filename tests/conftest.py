f6d5d0461df0e93eedabbfd6e3bf275833277e32
Benjamin Fockter <benjamin.fkt@gmail.com>
2025-01-21 18:18:08 +0000

    finish unit tests for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ import pytest
 
 @pytest.fixture(scope="session")
 def default_hackernews_front_webpage():
-    hn_webpage = os.path.join(
-        os.path.dirname(__file__), "fixtures", "index.html"
+    file_path = os.path.join(
+        os.path.dirname(__file__), "fixtures", "hn_page_1.html"
     )
 
-    return hn_webpage
+    with open(file_path, "r", encoding="utf-8") as f:
+        return f.read()
 
 
--- tests/unit/test_scraper.py
+++ tests/unit/test_scraper.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 from bs4 import BeautifulSoup
 import pytest
+import requests
 
 from scraper.scraper import (
     WEBSITES,
@@ -10,9 +11,9 @@ from scraper.scraper import (
 
 
 @patch('scraper.scraper.requests.get')
-def test_fetch_website_success(mock_request_get, default_hackernews_front_webpage):
-    mock_request_get.return_value.status_code = 200
-    mock_request_get.return_value.text = default_hackernews_front_webpage
+def test_fetch_website_success(mock_requests_get, default_hackernews_front_webpage):
+    mock_requests_get.return_value.status_code = 200
+    mock_requests_get.return_value.text = default_hackernews_front_webpage
 
     parsed_website = fetch_website(WEBSITES[0])
     soup = BeautifulSoup(default_hackernews_front_webpage, 'html.parser')
@@ -21,12 +22,29 @@ def test_fetch_website_success(mock_request_get, default_hackernews_front_webpag
     assert parsed_website == soup
 
 
-def test_fetch_website_timout():
-    # TODO: this
-    pass
+@patch('scraper.scraper.requests.get')
+def test_fetch_website_timout(mock_requests_get, capfd):
+    mock_requests_get.side_effect = requests.exceptions.Timeout
 
+    result = fetch_website(WEBSITES[0])
 
-# def test_fetch_website_bad_response():
-#     pass
+    mock_requests_get.assert_called_once_with(WEBSITES[0], timeout=5)
+    assert result is None
+
+    out, err = capfd.readouterr()
+    assert "Error fetching the website" in out
+    
+
+@patch('scraper.scraper.requests.get')
+def test_fetch_website_bad_response(mock_requests_get, capfd):
+    mock_requests_get.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError("404 Error")
+    mock_requests_get.return_value.text = "Not Found"
+
+    result = fetch_website(WEBSITES[0])
+
+    assert result is None
+    
+    out, err = capfd.readouterr()
+    assert "Error fetching the website" in out
 
 

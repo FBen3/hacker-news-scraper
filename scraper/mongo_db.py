0f8561f155d67de211d7c3f7e64135f501f0d635
Benjamin Fockter <benjamin.fkt@gmail.com>
2025-01-30 22:18:44 +0000

    Add Error handling to mongo_db.py

--- scraper/mongo_db.py
+++ scraper/mongo_db.py
@@ -11,6 +11,7 @@ import json
 
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.errors import PyMongoError
 
 from config import (
     MONGO_ATLAS_URI,
@@ -30,43 +31,68 @@ logger = logging.getLogger(__name__)
 @contextmanager
 def connect_database():
     """Create a context manager for database connections"""
-    client = MongoClient(MONGO_ATLAS_URI, server_api=ServerApi('1'))
-
     try:
+        client = MongoClient(MONGO_ATLAS_URI, server_api=ServerApi('1'), serverSelectionTimeoutMS=5000)
+        client.admin.command('ping')  # check if database is reachable
         yield client[DATABASE_NAME]
+
+    except Exception as e:
+        logger.error(f"[From MongoDB] Failed to connect to database: {e}")
+        yield None  # returning None so callers can handle the failure (instead of program crashing)
+
     finally:
         client.close()
 
 
 def fetch_saved_articles(date=None):
     """Get every saved article by default; filter by date if provided"""
-    with connect_database() as db:
-        collection = db[COLLECTION_NAME]
+    try:
+        with connect_database() as db:
+            if db is None:
+                return json.dumps({"error": "Database connection failed"})
 
-        pipeline = []  # initialize pipeline
-        if date:
-            pipeline.append(
-                {"$match": {"scrape_date": {"$regex": f"^{date}"}}}  # match documents with a date if present
-            )
-        pipeline.append({"$unwind": "$saves"})  # unwind the saves array
-        pipeline.append({"$replaceRoot": {"newRoot": "$saves"}})  # project only article detials
+            collection = db[COLLECTION_NAME]
 
-        all_articles = list(collection.aggregate(pipeline))
+            pipeline = []  # initialize pipeline
+            if date:
+                pipeline.append(
+                    {"$match": {"scrape_date": {"$regex": f"^{date}"}}}  # match documents with a date if present
+                )
+            pipeline.append({"$unwind": "$saves"})  # unwind the saves array
+            pipeline.append({"$replaceRoot": {"newRoot": "$saves"}})  # project only article detials
 
-        return json.dumps(all_articles, indent=4)
+            all_articles = list(collection.aggregate(pipeline))
+
+            return json.dumps(all_articles, indent=4)
+        
+    except PyMongoError as e:
+        logger.error(f"[From MongoDB] Failed to fetch saved articles: {e}")
+        return json.dumps({"error": "Database query failed for some reason!"})
 
 
 def get_original_save_date(title):
     """Get the original scrape date of an article"""
-    with connect_database() as db:
-        collection = db[COLLECTION_NAME]
+    try:    
+        with connect_database() as db:
+            if db is None:
+                return None
 
-        original_document = collection.find_one(
-            {"saves.title": title},  # query
-            {"scrape_date": 1, "_id": 0}  # projection
-        )
+            collection = db[COLLECTION_NAME]
 
-        return original_document["scrape_date"]
+            original_document = collection.find_one(
+                {"saves.title": title},  # query
+                {"scrape_date": 1, "_id": 0}  # projection
+            )
+
+            if not original_document:
+                logger.warning(f"[From MongoDB] No original save date found for: {title}")
+                return None
+
+            return original_document["scrape_date"]
+        
+    except PyMongoError as e:
+        logger.error(f"[From MongoDB] Failed to get original scrape date for: {title}. Error {e}")
+        return None
 
 
 def update_existing_article(article, title, original_date, update_date, collection):
@@ -87,45 +113,59 @@ def insert_data(data, duplicates=None):
     """Insert new articles; update saved ones"""
     update_date = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
-    with connect_database() as db:    
-        collection = db[COLLECTION_NAME]
+    try:
+        with connect_database() as db:    
+            if db is None:
+                return  # stop execution
 
-        if duplicates:
-            de_dupped_articles = [
-                article for article in data["saves"] if article["title"] not in duplicates
-            ]
+            collection = db[COLLECTION_NAME]
 
-            for article in data["saves"]:
-                title = article["title"]
-                if article["title"] in duplicates:
-                    original_date = get_original_save_date(title=title)
-                    update_existing_article(article, title, original_date, update_date, collection)
+            if duplicates:
+                de_dupped_articles = [
+                    article for article in data["saves"] if article["title"] not in duplicates
+                ]
 
-            data["saves"] = de_dupped_articles
-            logger.info(f"Updated {len(duplicates)} existing articles in database")
+                for article in data["saves"]:
+                    title = article["title"]
+                    if article["title"] in duplicates:
+                        original_date = get_original_save_date(title=title)
+                        update_existing_article(article, title, original_date, update_date, collection)
 
-        collection.insert_one(data)
-        logger.info(f"Inserted {len(data["saves"])} new articles in database")
+                data["saves"] = de_dupped_articles
+                logger.info(f"Updated {len(duplicates)} existing articles in database")
+
+            collection.insert_one(data)
+            logger.info(f"Inserted {len(data["saves"])} new articles in database")
+
+    except PyMongoError as e:
+        logger.error(f"[From MongoDB] Failed to insert/update data: {e}")
 
 
 def fetch_duplicate_titles(data, date):
     """Return articles already in the database"""
     current_titles = {article["title"] for article in data["saves"]}
 
-    with connect_database() as db:
-        collection = db[COLLECTION_NAME]
+    try:
+        with connect_database() as db:
+            if db is None:
+                return set()
 
-        pipeline = [
-            {"$match": {"scrape_date": {"$regex": f"^{date}"}}},  # match documents for a specific date
-            {"$unwind": "$saves"},  # unwind saved articles
-            {"$project": {"_id": 0, "title": "$saves.title"}}  # filter for only the title field
-        ]
-        result = collection.aggregate(pipeline)
+            collection = db[COLLECTION_NAME]
 
-        past_titles = {article["title"] for article in result}
-        duplicates = current_titles & past_titles
+            pipeline = [
+                {"$match": {"scrape_date": {"$regex": f"^{date}"}}},  # match documents for a specific date
+                {"$unwind": "$saves"},  # unwind saved articles
+                {"$project": {"_id": 0, "title": "$saves.title"}}  # filter for only the title field
+            ]
+            result = collection.aggregate(pipeline)
 
-        return duplicates
+            past_titles = {article["title"] for article in result}
+
+            return current_titles & past_titles
+        
+    except PyMongoError as e:
+        logger.error(f"[From MongoDB] Failed to fetch duplicate titles: {e}")
+        return set()
 
 
 def save_scraped_data(data):
@@ -134,22 +174,29 @@ def save_scraped_data(data):
         logger.warning("No scrape data to insert. Skipping database.")
         return
     
-    with connect_database() as db:
-        collection = db[COLLECTION_NAME]
+    try:
+        with connect_database() as db:
+            if db is None:
+                return
 
-        # check if database is empty for that day
-        # IF not empty -> check for duplicates
-        # ELSE -> insert data right away
-        today = datetime.now().strftime('%Y-%m-%d')
-        query = {"scrape_date": {"$regex": f"^{today}"}}
+            collection = db[COLLECTION_NAME]
 
-        if collection.count_documents(query) > 0:
-            duplicates = fetch_duplicate_titles(data, today)
-            insert_data(data, duplicates)
-            logger.info(f"Scraped data for {today}. {len(duplicates)} duplicates found.")
-        else:
-            insert_data(data)
-            logger.info(f"Scraped data for {today}")
+            # check if database is empty for that day
+            # IF not empty -> check for duplicates
+            # ELSE -> insert data right away
+            today = datetime.now().strftime('%Y-%m-%d')
+            query = {"scrape_date": {"$regex": f"^{today}"}}
+
+            if collection.count_documents(query) > 0:
+                duplicates = fetch_duplicate_titles(data, today)
+                insert_data(data, duplicates)
+                logger.info(f"Scraped data for {today}. {len(duplicates)} duplicates found.")
+            else:
+                insert_data(data)
+                logger.info(f"Scraped data for {today}")
+
+    except PyMongoError as e:
+        logger.error(f"[From MongoDB] Failed to save scraped data: {e}")
 
 
 if __name__ == "__main__":

a53061b98aa74081bcb44f6157af5d2dc28bb2a8
Benjamin Fockter <benjamin.fkt@gmail.com>
2025-02-19 21:35:49 +0000

    Start Flask integration

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+from scraper.scraper import parse_pages
+from scraper.mongo_db import save_scraped_data, fetch_saved_articles
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({"message": "This is Ben's Hacker News custom scraper API!"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
